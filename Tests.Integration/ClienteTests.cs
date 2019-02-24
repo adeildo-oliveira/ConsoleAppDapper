578dec62e68f351a3b183466346ac4182e7c0b32
Adeildo José de Oliveira <adeildo.joliveira@hotmail.com>
2019-02-24 16:50:37 -0300

    Infra estrutura de teste

--- Tests.Integration/ClienteTests.cs
+++ Tests.Integration/ClienteTests.cs
@@ -156,6 +156,8 @@ namespace Tests.Integration
 
             resultado.Should().HaveCount(1);
             resultado.Should().BeEquivalentTo(clienteBuilder);
+            resultado[0].Enderecos.Should().BeEquivalentTo(clienteBuilder.Enderecos);
+            resultado[0].Telefones.Should().BeEquivalentTo(clienteBuilder.Telefones);
             resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder.Id).Should().BeEquivalentTo(enderecoBuilder);
             resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder2.Id).Should().BeEquivalentTo(enderecoBuilder2);
             resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder3.Id).Should().BeEquivalentTo(enderecoBuilder3);
@@ -202,17 +204,171 @@ namespace Tests.Integration
 
             clienteBuilder.AdicionarEndereco(enderecoBuilder);
             clienteBuilder.AdicionarTelefone(telefoneBuilder);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder2);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder3);
 
             var resultado = (await _clienteRepository.ObterClientes()).ToList();
 
             resultado.Should().HaveCount(1);
             resultado.Should().BeEquivalentTo(clienteBuilder);
+            resultado[0].Enderecos.Should().BeEquivalentTo(clienteBuilder.Enderecos);
+            resultado[0].Telefones.Should().BeEquivalentTo(clienteBuilder.Telefones);
             resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder.Id).Should().BeEquivalentTo(enderecoBuilder);
             resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder.Id).Should().BeEquivalentTo(telefoneBuilder);
             resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder2.Id).Should().BeEquivalentTo(telefoneBuilder2);
             resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder3.Id).Should().BeEquivalentTo(telefoneBuilder3);
             resultado[0].Enderecos.Should().HaveCount(1);
             resultado[0].Telefones.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public async Task DeveBuscarTodosOsClienteComMaisDeUmEnderecoEMaisDeUmTelefone()
+        {
+            var clienteBuilder = new ClienteBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNome("Petit")
+                .ComSobreNome("Gateau")
+                .Criar();
+
+            var enderecoBuilder = new EnderecoBuilder()
+                .ComId(Guid.NewGuid())
+                .ComLogradouro("Av. 23 de Maio")
+                .ComBairro("Liberdade")
+                .ComCidade("São Paulo")
+                .ComEstado("SP")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var enderecoBuilder2 = new EnderecoBuilder()
+                .ComId(Guid.NewGuid())
+                .ComLogradouro("Av. Angélica")
+                .ComBairro("Consolação")
+                .ComCidade("São Paulo")
+                .ComEstado("SP")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var enderecoBuilder3 = new EnderecoBuilder()
+                .ComId(Guid.NewGuid())
+                .ComLogradouro("Av. Angélica")
+                .ComBairro("Pacaembu")
+                .ComCidade("São Paulo")
+                .ComEstado("SP")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var telefoneBuilder = new TelefoneBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNumero("123456789")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var telefoneBuilder2 = new TelefoneBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNumero("987654321")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var telefoneBuilder3 = new TelefoneBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNumero("147852369")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            clienteBuilder.AdicionarEndereco(enderecoBuilder);
+            clienteBuilder.AdicionarEndereco(enderecoBuilder2);
+            clienteBuilder.AdicionarEndereco(enderecoBuilder3);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder2);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder3);
+
+            var resultado = (await _clienteRepository.ObterClientes()).ToList();
+
+            resultado.Should().HaveCount(1);
+            resultado.Should().BeEquivalentTo(clienteBuilder);
+            resultado[0].Enderecos.Should().BeEquivalentTo(clienteBuilder.Enderecos);
+            resultado[0].Telefones.Should().BeEquivalentTo(clienteBuilder.Telefones);
+            resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder.Id).Should().BeEquivalentTo(enderecoBuilder);
+            resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder2.Id).Should().BeEquivalentTo(enderecoBuilder2);
+            resultado[0].Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder3.Id).Should().BeEquivalentTo(enderecoBuilder3);
+            resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder.Id).Should().BeEquivalentTo(telefoneBuilder);
+            resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder2.Id).Should().BeEquivalentTo(telefoneBuilder2);
+            resultado[0].Telefones.FirstOrDefault(x => x.Id == telefoneBuilder3.Id).Should().BeEquivalentTo(telefoneBuilder3);
+            resultado[0].Enderecos.Should().HaveCount(3);
+            resultado[0].Telefones.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public async Task DeveBuscarMaisDeUmClienteComUmEnderecoEUmTelefone()
+        {
+            var clienteBuilder = new ClienteBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNome("Petit")
+                .ComSobreNome("Gateau")
+                .Criar();
+
+            var telefoneBuilder = new TelefoneBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNumero("123456789")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var enderecoBuilder = new EnderecoBuilder()
+                .ComId(Guid.NewGuid())
+                .ComLogradouro("Av. Angélica")
+                .ComBairro("Consolação")
+                .ComCidade("São Paulo")
+                .ComEstado("SP")
+                .ComCliente(clienteBuilder)
+                .Criar();
+
+            var clienteBuilder2 = new ClienteBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNome(".Net")
+                .ComSobreNome("Core")
+                .Criar();
+
+            var enderecoBuilder2 = new EnderecoBuilder()
+                .ComId(Guid.NewGuid())
+                .ComLogradouro("Av. 23 de Maio")
+                .ComBairro("Liberdade")
+                .ComCidade("São Paulo")
+                .ComEstado("SP")
+                .ComCliente(clienteBuilder2)
+                .Criar();
+
+            var telefoneBuilder2 = new TelefoneBuilder()
+                .ComId(Guid.NewGuid())
+                .ComNumero("987654321")
+                .ComCliente(clienteBuilder2)
+                .Criar();
+
+            clienteBuilder.AdicionarEndereco(enderecoBuilder);
+            clienteBuilder.AdicionarTelefone(telefoneBuilder);
+            clienteBuilder2.AdicionarEndereco(enderecoBuilder2);
+            clienteBuilder2.AdicionarTelefone(telefoneBuilder2);
+
+            var resultado = (await _clienteRepository.ObterClientes()).ToList();
+
+            resultado.Should().HaveCount(2);
+
+            var resultadoCliente = resultado.FirstOrDefault(x => x.Id == clienteBuilder.Id);
+            resultadoCliente.Should().BeEquivalentTo(clienteBuilder);
+            resultadoCliente.Enderecos.Should().BeEquivalentTo(clienteBuilder.Enderecos);
+            resultadoCliente.Telefones.Should().BeEquivalentTo(clienteBuilder.Telefones);
+            resultadoCliente.Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder.Id).Should().BeEquivalentTo(enderecoBuilder);
+            resultadoCliente.Telefones.FirstOrDefault(x => x.Id == telefoneBuilder.Id).Should().BeEquivalentTo(telefoneBuilder);
+            resultadoCliente.Enderecos.Should().HaveCount(1);
+            resultadoCliente.Telefones.Should().HaveCount(1);
+
+            resultadoCliente = resultado.FirstOrDefault(x => x.Id == clienteBuilder2.Id);
+            resultadoCliente.Should().BeEquivalentTo(clienteBuilder2);
+            resultadoCliente.Enderecos.Should().BeEquivalentTo(clienteBuilder2.Enderecos);
+            resultadoCliente.Telefones.Should().BeEquivalentTo(clienteBuilder2.Telefones);
+            resultadoCliente.Enderecos.FirstOrDefault(x => x.Id == enderecoBuilder2.Id).Should().BeEquivalentTo(enderecoBuilder2);
+            resultadoCliente.Telefones.FirstOrDefault(x => x.Id == telefoneBuilder2.Id).Should().BeEquivalentTo(telefoneBuilder2);
+            resultadoCliente.Enderecos.Should().HaveCount(1);
+            resultadoCliente.Telefones.Should().HaveCount(1);
         }
     }
 }
--- Tests.Integration/Tools/IntegrationTestFixture.cs
+++ Tests.Integration/Tools/IntegrationTestFixture.cs
@@ -1,9 +1,16 @@
-﻿using ConsoleApp.InfraData;
+﻿using Castle.Core.Configuration;
+using ConsoleApp.InfraData;
 using ConsoleApp.InfraData.Context;
 using Dapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Tests.Integration.Tools
 {
@@ -11,6 +18,7 @@ namespace Tests.Integration.Tools
     {
         private readonly ServiceCollection _serviceCollection;
         private readonly ServiceProvider _serviceProvider;
+        private static string connectionString;
 
         public IntegrationTestFixture()
         {
@@ -18,16 +26,73 @@ namespace Tests.Integration.Tools
             _serviceCollection = _serviceCollection ?? new ServiceCollection();
             _serviceCollection.AddScoped<IClienteRepository, ClienteRepository>();
 
-            _serviceProvider = _serviceCollection.BuildServiceProvider();   
+            _serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build()
+                .GetConnectionString("DefaultConnection")
+                .Replace("AppConsoleDapper_Integration", "Master");
+
+            if (!ExisteDataBase())
+                CreateDataBase();
+
+            if(!ExisteTables())
+                CreateTables();
         }
 
         public T GetService<T>()
         {
-            ClearDataBase();
+            ClearTables();
             return _serviceProvider.GetService<T>();
         }
 
-        private void ClearDataBase()
+        private static bool ExisteDataBase()
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var resultado = conn.QueryFirstOrDefault($"SELECT name as Nome FROM master.dbo.sysdatabases where name = '{GetConnectionDapper.Connection.Database}'", 
+                    commandType: CommandType.Text);
+
+                return resultado == null ? false : true;
+            }
+        }
+
+        private static void CreateDataBase()
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                conn.Query($"CREATE DATABASE {GetConnectionDapper.Connection.Database}", commandType: CommandType.Text);
+            }
+        }
+
+        private static bool ExisteTables()
+        {
+            using (var conn = GetConnectionDapper.Connection)
+            {
+                conn.Open();
+                var resultado = conn.QueryFirstOrDefault(
+                    $"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_CATALOG = '{GetConnectionDapper.Connection.Database}'",
+                    commandType: CommandType.Text);
+
+                return resultado == null ? false : true;
+            }
+        }
+
+        private static void CreateTables()
+        {
+            var file = Directory.GetFiles($"{Environment.CurrentDirectory}\\Tools").FirstOrDefault(x => x.Contains(".sql"));
+            var scriptSQL = File.ReadAllText(file);
+
+            using (var conn = GetConnectionDapper.Connection)
+            {
+                conn.Open();
+                conn.Query(scriptSQL, commandType: CommandType.Text);
+            }
+        }
+
+        private static void ClearTables()
         {
             using (var conn = GetConnectionDapper.Connection)
             {
@@ -44,6 +109,7 @@ namespace Tests.Integration.Tools
         {
             _serviceProvider.Dispose();
             _serviceCollection.Clear();
+            GetConnectionDapper.Connection.Dispose();
             GC.SuppressFinalize(this);
         }
     }
